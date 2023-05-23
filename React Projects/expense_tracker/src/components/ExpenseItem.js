1f3db035c744b185c079a8c0b31e502423d240b8
swathin25 <sriswathin25@gmail.com>
2023-05-23 10:20:28 +0530

    Handled Delete

--- React Projects/expense_tracker/src/components/ExpenseItem.js
+++ React Projects/expense_tracker/src/components/ExpenseItem.js
@@ -1,13 +1,12 @@
 export default function ExpenseItem(props){
+    const deleteItem  = ()=>{props.handleDelete(props.index)}
     return(
         <div class="card p-2">
         <h2>{props.title}</h2>
         <p>{props.on}</p>
         <p>{props.amount}</p>
         <div class="text-right">
-        <input class="btn btn-warning" type="button" value="Edit" />
-        <span class="p-2"></span>
-        <input class="btn btn-danger" type="button" value="Delete" />        
+        <input class="btn btn-danger" type="button" value="Delete" onClick={deleteItem} />        
         </div>
         </div>
     )
